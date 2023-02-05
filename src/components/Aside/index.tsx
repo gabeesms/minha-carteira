646fa251ba310739806a82d2e88f5560f0fad89e
Gabrielle Maciel <gabriellemacieel@gmail.com>
2023-02-05 19:47:24 -0400

    menu lateral

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
-import { Container } from './styles';
+import {
+    MdDashboard,
+    MdArrowDownward,
+    MdArrowUpward,
+    MdExitToApp
+} from 'react-icons/md';
+import LogoImg from '../../assets/logo.svg';
+import { Container, Header, LogImg, Title, MenuContainer, MenuItemLink } from './styles';
 
 const Aside: React.FC = () => {
     return(
         <Container>
-            <h1>Aside</h1>
+         <Header>
+            <LogImg src={LogoImg} alt="Logo Minha Carteira"/>
+            <Title>Minha Carteira</Title>
+         </Header>
+         <MenuContainer>
+            <MenuItemLink href="#">
+                <MdDashboard/>
+                Dashboard
+            </MenuItemLink>
+            <MenuItemLink href="#">
+            <MdArrowUpward/>
+                Entradas
+            </MenuItemLink>
+            <MenuItemLink href="#">
+            <MdArrowDownward/>
+                Saídas
+            </MenuItemLink>
+            <MenuItemLink href="#">
+            <MdExitToApp/>
+                Sair
+            </MenuItemLink>
+         </MenuContainer>
         </Container>
        
     );
---
+++ src/components/ContentHeader/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from './styles';
+
+const ContentHeader: React.FC = () => {
+    return(
+        <Container>
+            <h1>Content Header</h1>
+        </Container>
+       
+    );
+}
+
+export default ContentHeader;
